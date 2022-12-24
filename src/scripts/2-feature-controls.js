f4115ec466b04ab3b8455e2b4d7f47a496b2b6af
Zyruks <kuzenshi@gmail.com>
2022-12-24 14:05:26 -0400

    refactor: :recycle: Re-organize js code and modify how i made feature control animations

--- src/scripts/2-feature-controls.js
+++ src/scripts/2-feature-controls.js
@@ -18,56 +18,29 @@ function classCleaner(tag) {
   }, 380);
 }
 
-function carouselActivity(active, inactiveOne, inactiveTwo, animation) {
-  carousel[active].classList.add("dp-grid");
-  setTimeout(() => {
-    carousel[inactiveOne].classList.remove("dp-grid");
-    carousel[inactiveTwo].classList.remove("dp-grid");
-  }, 370);
-
-  //? Animations
-  for (let i = 0; i <= 2; i++) {
-    if (
-      active === i &&
-      carousel[inactiveOne].classList.contains("dp-grid") === true
-    ) {
-      carousel[active].classList.add(featureControlAnimations.in[animation]);
-      carousel[inactiveOne].classList.add(
-        featureControlAnimations.out[animation]
-      );
-    } else {
-      carousel[active].classList.add(featureControlAnimations.in[animation]);
-      carousel[inactiveTwo].classList.add(
-        featureControlAnimations.out[animation]
-      );
-    }
-  }
-}
-
 function featureSelection(active, inactiveOne, inactiveTwo, animation) {
   featureControls[active].addEventListener("click", () => {
     if (featureControls[active].getAttribute("data-state") !== "active") {
-      featureControls[active].setAttribute("data-state", "active");
-      featureControls[inactiveOne].setAttribute("data-state", "inactive");
-      featureControls[inactiveTwo].setAttribute("data-state", "inactive");
+      featureControls[active].setAttribute("data-state", "willBeActive");
+      if (window.innerWidth < 1025) {
+        featureAnimationMobile();
+      } else {
+        featureAnimationDesktop();
+      }
+      setTimeout(() => {
+        featureControls[inactiveOne].setAttribute("data-state", "inactive");
+        featureControls[inactiveTwo].setAttribute("data-state", "inactive");
+        featureControls[active].setAttribute("data-state", "active");
+      }, 350);
 
       // ? Carousel
-      if (
-        active === 1 &&
-        carousel[inactiveTwo].classList.contains("dp-grid") === true
-      ) {
-        carouselActivity(active, inactiveOne, inactiveTwo, 0);
-      } else {
-        carouselActivity(active, inactiveOne, inactiveTwo, animation);
-      }
 
-      classCleaner(carousel);
+      carouselActivity(active, inactiveOne, inactiveTwo);
     }
   });
 }
 
-featureSelection(0, 1, 2, 0);
+featureSelection(0, 1, 2);
+featureSelection(1, 0, 2);
 
-featureSelection(1, 0, 2, 1);
-
-featureSelection(2, 1, 0, 1);
+featureSelection(2, 1, 0);
