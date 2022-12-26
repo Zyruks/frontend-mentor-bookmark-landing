9626991743fc44923dc80b2981ca93482d44d3c9
Zyruks <kuzenshi@gmail.com>
2022-12-25 23:40:09 -0400

    build: :truck: Rename first script

--- src/scripts/1-burger-menu.js
+++ src/scripts/1-burger-menu.js
@@ -2,13 +2,16 @@
 
 const burgerIcon = document.querySelector(".js-burger-icon");
 const primaryNav = document.querySelector(".js-primary-nav");
+const primaryNavLogo = document.querySelector(".js-logo");
 
 burgerIcon.addEventListener("click", () => {
   if (primaryNav.classList.contains("dp-flex") !== true) {
     primaryNav.classList.add("dp-flex");
     burgerIcon.setAttribute("src", "./assets/svg/icon-close.svg");
+    primaryNavLogo.setAttribute("data-state", "open");
   } else {
     primaryNav.classList.remove("dp-flex");
     burgerIcon.setAttribute("src", "./assets/svg/icon-hamburger.svg");
+    primaryNavLogo.setAttribute("data-state", "close");
   }
 });
